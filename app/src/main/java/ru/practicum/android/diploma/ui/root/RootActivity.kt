ebc4d67fb58c5a444ab6f50fc15e9ae5307870d8
aleksandrpopov <alexpopov06@mail.ru>
2026-04-20 10:54:38 +0300

    базовая навигация + фрагменты заглушки

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -3,13 +3,20 @@ package ru.practicum.android.diploma.ui.root
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavController()
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         // Пример использования access token для HeadHunter API
@@ -18,6 +25,11 @@ class RootActivity : AppCompatActivity() {
 
     private fun networkRequestExample(accessToken: String) {
         // ...
+    }
+    private fun setupNavController() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHost.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
 }
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancySearchFragment.kt
@@ -0,0 +1,17 @@
+package ru.practicum.android.diploma.ui.vacancy
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.R
+
+class VacancySearchFragment : Fragment(R.layout.fragment_vacancy_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.filterButton).setOnClickListener {
+            findNavController().navigate(R.id.action_vacancySearchFragment_to_filterFragment)
+        }
+    }
+}
