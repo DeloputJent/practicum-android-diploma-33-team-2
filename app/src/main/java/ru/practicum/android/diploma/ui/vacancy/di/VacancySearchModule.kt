ce5b6b18395807de3c5288f31645f4d65fec83a2
aleksandrpopov <alexpopov06@mail.ru>
2026-04-30 23:13:47 +0300

    фикс detect

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/di/VacancySearchModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/di/VacancySearchModule.kt
@@ -1,19 +1,14 @@
 package ru.practicum.android.diploma.ui.vacancy.di
 
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.qualifier.named
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.ui.vacancy.VacancySearchViewModel
 
 val vacancySearchModule = module {
-    single(named("searchDebounceMs")) {
+    single {
         androidContext().resources.getInteger(R.integer.search_debounce_ms).toLong()
     }
-    org.koin.androidx.viewmodel.dsl.viewModel {
-        VacancySearchViewModel(
-            searchInteractor = get(),
-            debounceMs = get(named("searchDebounceMs")),
-        )
-    }
+    viewModelOf(::VacancySearchViewModel)
 }
