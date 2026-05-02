4466365f379df92c406ced60ff6ca2535510c3bd
aleksandrpopov <alexpopov06@mail.ru>
2026-05-02 19:42:00 +0300

    обработка ошибок

--- app/src/main/java/ru/practicum/android/diploma/data/network/SearchRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/SearchRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package ru.practicum.android.diploma.data.network
 
 import com.google.gson.Gson
+import com.google.gson.JsonIOException
+import java.io.IOException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import ru.practicum.android.diploma.BuildConfig
@@ -8,6 +10,7 @@ import ru.practicum.android.diploma.data.converters.SearchDtoConverter
 import ru.practicum.android.diploma.data.dto.search.VacanciesSearchResponseDto
 import ru.practicum.android.diploma.domain.search.api.SearchRepository
 import ru.practicum.android.diploma.domain.search.models.SearchResult
+import ru.practicum.android.diploma.util.ErrorKind
 import ru.practicum.android.diploma.util.Resource
 
 class SearchRepositoryImpl(
@@ -25,18 +28,22 @@ class SearchRepositoryImpl(
                 )
 
                 if (!response.isSuccessful) {
-                    return@withContext Resource.Error<SearchResult>()
+                    return@withContext Resource.Error<SearchResult>(ErrorKind.SERVER)
                 }
 
                 val json = response.body()?.string()
                 if (json.isNullOrEmpty()) {
-                    return@withContext Resource.Error<SearchResult>()
+                    return@withContext Resource.Error<SearchResult>(ErrorKind.SERVER)
                 }
 
                 val dto = gson.fromJson(json, VacanciesSearchResponseDto::class.java)
                 Resource.Success(converter.map(dto))
+            } catch (_: JsonIOException) {
+                Resource.Error<SearchResult>(ErrorKind.SERVER)
+            } catch (_: IOException) {
+                Resource.Error<SearchResult>(ErrorKind.NO_INTERNET)
             } catch (_: Exception) {
-                Resource.Error<SearchResult>()
+                Resource.Error<SearchResult>(ErrorKind.SERVER)
             }
         }
     }
